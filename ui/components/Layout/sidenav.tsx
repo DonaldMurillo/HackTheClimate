6912f558c6950ab8186250932966a574d812832b
Donald Murillo <donald.murillo07@gmail.com>
2021-03-24 09:13:55 -0400

    - added generic form

--- ui/components/Layout/sidenav.tsx
+++ ui/components/Layout/sidenav.tsx
@@ -1,6 +1,6 @@
 import { Box, Button, VStack } from "@chakra-ui/react";
-import CollectionsBookmarkIcon from '@material-ui/icons/CollectionsBookmark';
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import EventIcon from '@material-ui/icons/Event';
+import HomeIcon from '@material-ui/icons/Home';
 import React, { useContext, useEffect } from "react";
 import { useRouter } from "next/dist/client/router";
 import AppContext from "../../state/AppContext";
@@ -10,8 +10,8 @@ import { DASHBOARD_URL } from "../../utils/urls";
 export default function Sidenav() {
     const { currentRoute, getCurrentRoute } = useContext(AppContext);
     const router = useRouter();
-    const route = router.route.replace('/dashboard/', '')
-    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, route: 'profile' | 'catalogs') => {
+    const route = router.route.replace('/dashboard/', '').replace('/new', '')
+    const handleClick = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, route: 'summary' | 'events') => {
       event.preventDefault();
       router.push(DASHBOARD_URL + route);
     }
@@ -23,7 +23,7 @@ export default function Sidenav() {
     
     <VStack h="inherit" spacing="4" minW="content" bg="#333" color="white" pr="15px" pl="5px">
       <Box as="h2" mt="5">My App</Box>
-      <Button 
+      {/* <Button 
         isFullWidth={true} 
         variant="link" 
         leftIcon={<AccountCircleIcon />}
@@ -35,20 +35,32 @@ export default function Sidenav() {
         onClick={(event) => handleClick(event, 'profile')}
         >
         Profile
+      </Button> */}
+        <Button 
+            isFullWidth={true} 
+            variant="link" 
+            leftIcon={<HomeIcon />}
+            p="10px" 
+            justifyContent="unset"
+            color={currentRoute == 'summary' ? '#82DD8D' : 'white'}
+            fontWeight="normal"
+            ml="10px"
+            onClick={(event) => handleClick(event, 'summary')}
+            >
+            Summary
       </Button>
       <Button         
         isFullWidth={true} 
-        variant="link" leftIcon={<CollectionsBookmarkIcon />}
+        variant="link" leftIcon={<EventIcon />}
         p="10px"
         justifyContent="unset"
-        color={currentRoute == 'catalogs' ? '#82DD8D' : 'white'}
+        color={currentRoute == 'events' ? '#82DD8D' : 'white'}
         fontWeight="normal"
         ml="10px"
-        onClick={(event) => handleClick(event, 'catalogs')}
+        onClick={(event) => handleClick(event, 'events')}
         >
-        <span>Others</span>
+        <span>Events</span>
       </Button>
     </VStack>
-
   )
 }
--- ui/pages/index.tsx
+++ ui/pages/index.tsx
@@ -1,8 +1,10 @@
 import Layout from '../components/Layout/layout'
 import utilStyles from '../styles/utils.module.scss'
 
+//TODO: REROUTE TO DASHBOARED SUMMARY OR LOGIN
 
 export default function Home() {
+
   return (
     <Layout>
       <div className={utilStyles.supa}>Boilerplate</div>
