16a280c78b338a4d4e87338e23636d820d074979
Gabester0 <gceipper@gmail.com>
2021-03-25 13:08:21 -0400

    cleaned up code

--- server/models/financial.schema.ts
+++ server/models/financial.schema.ts
@@ -4,13 +4,3 @@ import { Financial } from './financial.model';
 export const financialSchema = new mongoose.Schema<Financial>({
 
 });
-
-// The first section about Mongoose schemas might be useful.  It has a list of the data types in Mongoose
-// which should be the same as MongoDB (I found this a helpful refresher on this, lol)
-// https://mongoosejs.com/docs/guide.html#definition
-
-// I also referenced Donald's code he wrote with Mongoose here:
-// https://github.com/DonaldMurillo/express-test/blob/master/models/supplier.schema.ts
-
-// And a project I did with Mongoose:
-// https://github.com/Gabester0/Task-manager-api/tree/main/src/modelshttps://github.com/Gabester0/Task-manager-api/tree/main/src/models
